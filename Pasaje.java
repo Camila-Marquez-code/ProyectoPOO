b6f7f5c01bae38748b06c6e082aca8b80ab7f786
Milywiwu <camila.marquez2501@alumnos.ubiobio.cl>
2026-04-07 18:30:06 -0400

    Add files via upload

---
+++ Pasaje.java
@@ -0,0 +1,28 @@
+public class Pasaje {
+    private long numero;
+    private int asiento;
+
+    public Pasaje(int asiento, Viaje viaje, Pasajero pasajero, Venta venta) {
+        this.asiento = asiento;
+        this.viaje = viaje;
+        this.pasajero = pasajero;
+        this.venta = venta;
+    }
+
+    public long getNumero() {
+        return numero;
+    }
+
+    public int getAsiento() {
+        return asiento;
+    }
+
+    public Viaje getViaje() {
+        return viaje;
+    }
+
+    public Pasajero getPasajero() {
+        return pasajero;
+
+    }
+}
