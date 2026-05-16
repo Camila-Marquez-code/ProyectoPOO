8c6d1be06ac335e2899ac14783e0ae1a3b2b4561
marisolyb <marisol.yanez2501@alumnos.ubiobio.cl>
2026-05-16 13:37:51 -0400

    Add files via upload

---
+++ Direccion.java
@@ -0,0 +1,41 @@
+public class Direccion {
+    private String calle;
+    private int numero;
+    private String comuna;
+
+    public Direccion (String calle, int numero, String comuna){
+        this.calle=calle;
+        this.numero=numero;
+        this.comuna=comuna;
+    }
+
+    public String getCalle() {
+        return calle;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public String getComuna() {
+        return comuna;
+    }
+
+    public String toString() {
+        return "Calle: " + calle + "Número:" + numero + "Comuna: " + comuna;
+    }
+
+    @Override
+    public boolean equals(Object otro) {
+        if (this == otro) {
+            return true;
+        }
+        if (otro == null || getClass() != otro.getClass()) {
+            return false;
+        }
+        Direccion d = (Direccion) otro;
+        return numero == d.numero &&
+                calle.equals(d.calle) &&
+                comuna.equals(d.comuna);
+    }
+}
