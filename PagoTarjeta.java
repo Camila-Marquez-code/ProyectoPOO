03d2ba0c889994939759fa3c62248a2b474414b1
marisolyb <marisol.yanez2501@alumnos.ubiobio.cl>
2026-05-17 14:31:12 -0400

    Add files via upload

--- PagoTarjeta.java
+++ PagoTarjeta.java
@@ -4,9 +4,10 @@ public class PagoTarjeta extends Pago {
 
     private long nroTarjeta;
 
-    Public PagoTarjeta (int monto, long nroTarjeta){
-        this.nroTarjeta=nroTarjeta;
+    public PagoTarjeta (int monto, long nroTarjeta){
         super(monto);
+        this.nroTarjeta=nroTarjeta;
+
     }
 
     public long getNroTarjeta() {
