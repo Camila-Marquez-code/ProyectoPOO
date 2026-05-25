3e3287bbb847c1ba75b5ad70698cfc66151f1c5c
Milywiwu <camila.marquez2501@alumnos.ubiobio.cl>
2026-05-25 15:17:05 -0400

    agregar terminal

---
+++ modelo/Terminal.java
@@ -0,0 +1,46 @@
+//Camila Valeska Márquez Burgos
+package modelo;
+import java.util.ArrayList;
+import utilidades.*;
+
+public class Terminal {
+    private String nombre;
+    private Direccion direccion;
+    private ArrayList<Viaje> llegadas;
+    private ArrayList<Viaje> salidas;
+
+    public Terminal(String nombre, Direccion direccion) {
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.llegadas = new ArrayList<Viaje>();
+        this.salidas = new ArrayList<Viaje>();
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public Direccion getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(Direccion direccion) {
+        this.direccion = direccion;
+    }
+
+    public void addLLegada(Viaje viaje) {
+        llegadas.add(viaje);
+    }
+
+    public void addSalida(Viaje viaje) {
+        salidas.add(viaje);
+    }
+
+    public Viaje[] getLlegadas() {
+        return llegadas.toArray(new Viaje[0]);
+    }
+
+    public Viaje[] getSalidas() {
+        return salidas.toArray(new Viaje[0]);
+    }
+}
